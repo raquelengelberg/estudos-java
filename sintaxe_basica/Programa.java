77a06cc0948f23054db17494322b3f7e506aa34a
Raquel Engelberg <raquelcristina.francisco@gmail.com>
2022-01-10 16:21:26 -0300

    Teste variáveis

---
+++ sintaxe_basica/Programa.java
@@ -0,0 +1,10 @@
+package sintaxe_basica;
+
+public class Programa {
+    public static void main(String[] args) {
+        System.out.println("Ola senhora Raquel Engelberg");
+        int idade;
+        idade = 37;
+        System.out.println(idade + " anos");
+    }
+}
